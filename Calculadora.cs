701bcbf1c55c1d600c2aafba109b4e38b6695af2
Dioggs <118784708+Dioggs@users.noreply.github.com>
2023-01-20 16:10:00 -0300

    Add files via upload

---
+++ Calculadora.cs
@@ -0,0 +1,43 @@
+﻿using System.Drawing;
+
+public class program
+{
+    public static void Main()
+    {
+        Console.WriteLine("-------------------");
+        Console.WriteLine("Calculadora Digital");
+        Console.WriteLine("-------------------");
+
+        //Operação
+        Console.WriteLine("Gostaria de somar, substrair, multiplicar ou dividir?: ");
+        string operação = Console.ReadLine();
+
+        //Primeiro numero
+        Console.WriteLine("Fornece um numero: ");
+        int num1 = Convert.ToInt16(Console.ReadLine());
+
+        //Segundo numero
+        Console.WriteLine("Fornece outro numero: ");
+        int num2 = Convert.ToInt16(Console.ReadLine());
+
+
+        switch (operação)
+        {
+            case "somar":
+                Console.WriteLine($"a soma entre {num1} e {num2} é {num1 + num2}");
+                break;
+            case "substrair":
+                Console.WriteLine($"a substração entre {num1} e {num2} é {num1 - num2}");
+                break;
+            case "multiplicar":
+                Console.WriteLine($"a multiplicação entre {num1} e {num2} é {num1 * num2}");
+                break;
+            case "dividir":
+                Console.WriteLine($"a divisão entre {num1} e {num2} é {num1 / num2}");
+                break;
+            default:
+                Console.WriteLine("isso não é uma operação válida");
+                break;
+        }
+    }
+}
